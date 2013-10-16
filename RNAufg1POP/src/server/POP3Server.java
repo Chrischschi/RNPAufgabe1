1ef2c21e652210fad0ccd77e55bfcca34f086707
SWindrath <steffen.windrath@haw-hamburg.de>
2013-10-16 17:41:51 +0200

    Skelett der methode computeCommand hinzugefügt (kümmert sich um die vom client kommenden anfragen).

--- RNAufg1POP/src/server/POP3Server.java
+++ RNAufg1POP/src/server/POP3Server.java
@@ -1,5 +1,21 @@
 package server;
 
 public class POP3Server {
-
+	
+	public void computeCommand(String msg){
+		String cmd = msg.toUpperCase().substring(0, 3);
+		switch(cmd){
+		case("USER"):
+		case("PASS"):
+		case("QUIT"):
+		case("STAT"):
+		case("LIST"):
+		case("RETR"):
+		case("DELE"):
+		case("NOOP"):
+		case("RSET"):
+		case("UIDL"):
+		default: // Crash and Burn ;)
+		}
+	}
 }
