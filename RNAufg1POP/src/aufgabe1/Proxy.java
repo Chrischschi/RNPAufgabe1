8176f2ceec3f634a088b61c837fd0c953b8ca949
SWindrath <steffen.windrath@haw-hamburg.de>
2013-10-20 19:16:19 +0200

    Server fast fertig. Zum weitermachen fehlt IO.

--- RNAufg1POP/src/aufgabe1/Proxy.java
+++ RNAufg1POP/src/aufgabe1/Proxy.java
@@ -4,6 +4,8 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.wendholt.utility.SystemTrace;
 import de.wendholt.utility.Trace;
@@ -24,29 +26,30 @@ public class Proxy {
 	
 	private POP3Server server = new POP3Server();
 	
+	Timer timer = new Timer();
+	
 	private Trace sTrace = new SystemTrace();
 	
-	List<MailAccount> accountList = new ArrayList<MailAccount>();
+	List<MailAccount> externAccounts = new ArrayList<MailAccount>();
 	
 	public Proxy(){
 		String hostName = "pop3.test.net";
 		InetAddress address;
 		try {
 			address = InetAddress.getByName(hostName);
-			accountList.add(new MailAccount("TestName", "pass", address));
-			accountList.add(new MailAccount("abc", "def", address));
+			externAccounts.add(new MailAccount("TestName", "pass", address));
+			externAccounts.add(new MailAccount("abc", "def", address));
 		} catch (UnknownHostException e) {
 			sTrace.error("Unknown Host: " + hostName);
 		}
 		hostName = "pop3.web.de";
 		try {
 			address = InetAddress.getByName(hostName);
-			accountList.add(new MailAccount("TestName", "pass", address));
-			accountList.add(new MailAccount("abc", "def", address));
+			externAccounts.add(new MailAccount("TestName", "pass", address));
+			externAccounts.add(new MailAccount("abc", "def", address));
 		} catch (UnknownHostException e) {
 			sTrace.error("Unknown Host: " + hostName);
 		}
-		
 		
 		
 	}
@@ -55,6 +58,8 @@ public class Proxy {
 	
 	public void start(){
 		server.start();
+		//Alle 30 Sekunden mails holen
+		 timer.schedule(new MailTask(), 30*1000);
 	}
 	
 	public static void main(String[] args) {
@@ -62,4 +67,12 @@ public class Proxy {
 		proxy.start();
 	}
 
+	class MailTask extends TimerTask {
+        public void run() {
+        	sTrace.debug("Getting Mails");
+        	for (MailAccount acc:externAccounts){
+        		client.getMails(acc);
+        	}
+        }
+    }
 }
